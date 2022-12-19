0b23c0f7345fbc69e47dee5966cc907b7f63aed0
Haikal Khairul <48490386+haikalkhairul@users.noreply.github.com>
2022-12-19 01:36:32 +0000

    Add more items to blogs.js

--- src/data/blogs.js
+++ src/data/blogs.js
@@ -19,6 +19,13 @@ const blogs = {
             'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',
             'pic': 'https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
             'slug': 'how-it-all-started-1'
+        },
+        {
+            'title': 'How to build a brand',
+            'subtitle': '18th December 2022',
+            'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit.',
+            'pic': 'https://images.unsplash.com/photo-1589561253898-768105ca91a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+            'slug': 'how-it-all-started-2'
         }
     ]
 };
